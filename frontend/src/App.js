0ad540dae3f42e83488aea5a5edb305f2b4d6d81
Owen Sullivan <multiojuice@gmail.com>
2019-01-28 20:40:48 -0500

    Updated frontend portion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Wrapper, Input, RestButton, Label, RowContainer } from './StyledComponents'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      endpoint: '',
+      response: null
+    };
+  }
+
+  restRequest = (event) => {
+    const restMethod = event.target.id;
+    axios.get(`http://127.0.0.1:5000/${this.state.endpoint}`, {method: restMethod})
+      .then(response => console.warn(response))
+  }
+
+  changeEndpoint = (event) => {
+    this.setState({endpoint: event.target.value})
+  }
+
   render() {
+    console.warn(this.state.endpoint)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+        <RowContainer>
+          <Label>Current endpoint: </Label>
+          <Input onChange={this.changeEndpoint}/>
+        </RowContainer>
+        <RowContainer>
+          <Label>Trigger rest method call:</Label>
+          <RestButton onClick={this.restRequest} id="GET">GET</RestButton>
+          <RestButton onClick={this.restRequest} id="POST">POST</RestButton>
+        </RowContainer>
+      </Wrapper>
     );
   }
 }
---
+++ frontend/src/StyledComponents.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Label = styled.span`
+    font-size: 24px;
+    margin-right: 20px;
+`;
+
+export const Input = styled.input`
+    width: 60%;
+    height: 30px;
+    font-size: 20px;
+    text-align: center;
+`;
+
+export const RowContainer = styled.div`
+    margin-bottom: 30px;
+    display: flex;
+`;
+
+export const RestButton = styled.button`
+    border-radius: 5px;
+    height: 40px;
+    min-width: 60px;
+    margin-right: 10px;
+`;
